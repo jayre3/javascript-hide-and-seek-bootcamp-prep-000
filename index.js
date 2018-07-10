07e0208e3468f5a33413d9f46ecf9e784ddea3a9
Jay Singh <jeewandeep.singh@gmail.com>
2018-07-10 23:58:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,9 @@ function increaseRankBy(n) {
   for (var i=0; i<item.length; i++) {
     item[i].innerHTML = parseInt(item[i].innerHTML) + n;
 }
+
+function deepestChild() {
+  var parent = document.querySelector("#grand-node").querySelectorAll("div");
+  var child = parent[parent.length-1];
+  return child;
+} 
